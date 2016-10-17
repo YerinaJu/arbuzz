02f700278d9e17a9ffc61228456fe923f714750e
juyerina <j35148@gmail.com>
2016-10-17 18:53:32 +0900

    main 완성

--- js/main.js
+++ js/main.js
@@ -7,22 +7,6 @@
 	var ctrl = new ScrollMagic.Controller();
 
 
-	//carouselScene 설정
-	/*var carouselScene = new ScrollMagic.Scene({
-		'triggerElement': '.main-carousel',
-		'triggerHook': 0,
-		'duration': 500
-	})
-	.setClassToggle('.main-carousel', 'fade-in')
-	.addTo(ctrl) // scene에 controller 연결 
-	.addIndicators({
-		'name':'carousel',
-		'colorStart':'red',
-		'colorEnd': 'red'
-	});
-	carouselScene.setPin('.main-carousel',{'pushFollowers':false});*/
-
-
 	var sceneList = '.banner-wrapper, .banner, .tabs, .product-wrapper, .products-item, h2, .about-us, .from-the-blog, .blog-items, .footer'.split(', ');
 
 	sceneList.forEach(function(element, index){
